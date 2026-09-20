b462699da423c9e1a91a3c62df48effe6082adbd
aaron <1457786801@qq.com>
2026-09-20 21:47:46 +0800

    add daily item transfor method

---
+++ main.py
@@ -0,0 +1,21 @@
+import sex
+
+
+if __name__ == "__main__":
+    # sex.process_food()  # 加工
+    # sex.daily_event(1, 138)  # 每日采集狩猎钓鱼交换
+    sex.shouhuo()
+    # sex.transfer_daily_task_item()
+    # print(sex.LACK_ITEMS)
+    # sex.exec_task()
+    # sex.daily_event(2, 83)  # 商会存储物品
+    # from_accounts = ["shifangfozu55"]
+    # sex.tansform_item(from_accounts, "qiqiwo321")
+    # print("====================================================")
+    # for i in sex.account_list:
+    #     print(i)
+    # sex.open_box()
+    # sex.assign_money()  # 分配金币
+    # sleep(3000)
+    # sex.add_friends()
+
